13396ff045d534aa73d4fef407fc8a0fd2c2ce3c
Dasha Lary <dk2800@nyu.edu>
2020-09-10 13:48:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ end
 
 def assign_rooms(attendees)
   room_assignments = Array.new
-  attendees.each_with_index { |person, index| room_assignments << "Hello, #{person}! You'll be assigned to room #{person[i]}!" }
+  attendees.each_with_index { |person, index| room_assignments << "Hello, #{person}! You'll be assigned to room #{index}!" }
   return room_assignments
 end
