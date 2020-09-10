a74a8bfff530ce9586e801eb31419605b59cf3dd
Dasha Lary <dk2800@nyu.edu>
2020-09-10 13:43:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = Array.new 
+ attendees.each { |person| badges << badge_maker(person) }
+ return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = Array.new
+  attendees.each { |person| room_assignments << "Hello, #{person}! You'll be assigned to room #{person[i]}!" }
+  return room_assignments
+end
